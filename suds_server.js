7cf4838ee65529bd437380ffae667650bf298d45
Sean McGary <sean.mcgary@gmail.com>
2011-03-23 17:07:16 -0400

    added soap script status pushing

--- suds_server.js
+++ suds_server.js
@@ -3,16 +3,27 @@ var net = require('net');
 var stalls = {'south':{}, 'north': {}};
 var connected = {'south': false, 'north': false};
 
+var web_clients = new Array();
+
+Array.prototype.remove = function(from, to) {
+	var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
+};
+
 var server = net.createServer(function(socket){
 	var suds_id = null;
+	var is_client = false;
+	var client_id;
 	
 	socket.addListener('connect', function(){
-		socket.write('connected');
+		//socket.write('connected');
+		console.log('connected');
 	});
 	
 	socket.addListener('data', function(data){
 		var recv = JSON.parse(data.toString());
-		
+		//console.log(recv);
 		if(recv.opcode == 'connect')
 		{
 			connected[recv['suds_id']] = true;
@@ -24,7 +35,12 @@ var server = net.createServer(function(socket){
 				var info = {'name': d[1], 'status': 0};
 				stalls[recv['suds_id']][d[0]] = info;
 			}
-			console.log(recv['suds_id'] + ' connected');
+			//console.log(recv['suds_id'] + ' connected');
+			
+			var msg = JSON.stringify({'opcode': 'suds_status', 'status': connected});
+			push_to_web_clients(msg);
+			
+			
 			//console.log(stalls);
 			socket.write("ACK\n");
 		}
@@ -34,8 +50,19 @@ var server = net.createServer(function(socket){
 			var stall = recv.stall
 			//console.log(recv);
 			stalls[id][stall]['status'] = recv['status'];
-			console.log(stalls);
+			//console.log(stalls);
 			socket.write("ACK\n");			
+		}
+		else if(recv.opcode == 'web_connect')
+		{
+			console.log('web client connected');
+			is_client = true;
+			client_id = new Date().getTime();
+			//console.log(client_id);
+			web_clients.push({'client_id': client_id, 'sock': socket});
+			var msg = JSON.stringify({'opcode': 'suds_status', 'status': connected});
+			console.log(msg);
+			socket.write(msg);
 		}
 		else
 		{
@@ -48,17 +75,63 @@ var server = net.createServer(function(socket){
 	socket.addListener('end', function(){
 		console.log('connection terminated');
 		// mark as disconnected
-		connected[suds_id] = false;
-		suds_id = null;
+		if(is_client == true)
+		{
+			console.log('client ended');
+			for(var i = 0; i < web_clients.length; i++)
+			{
+				if(web_clients[i]['client_id'] == client_id)
+				{
+					web_clients.remove(i);
+				}
+			}
+		}
+		else
+		{
+			console.log('suds ended');
+			connected[suds_id] = false;
+			suds_id = null;
+			var msg = JSON.stringify({'opcode': 'suds_status', 'status': connected});
+			push_to_web_clients(msg);
+		}
+		
 	});
 	
 	socket.addListener('error', function(e){
 		// mark as disconnected
-		connected[suds_id] = false;
-		stalls[suds_id] = {};
-		console.log(suds_id + " disconnected");
-		suds_id = null;		
+		if(is_client == true)
+		{
+			console.log('client ended');
+			for(var i = 0; i < web_clients.length; i++)
+			{
+				if(web_clients[i]['client_id'] == client_id)
+				{
+					web_clients.remove(i);
+				}
+			}
+		}
+		else
+		{
+			console.log('suds ended');
+			connected[suds_id] = false;
+			stalls[suds_id] = {};
+			console.log(suds_id + " disconnected");
+			suds_id = null;		
+			var msg = JSON.stringify({'opcode': 'suds_status', 'status': connected});
+			push_to_web_clients(msg);
+		}
+		
 	});
+	
+	function push_to_web_clients(message)
+	{
+		console.log('sending');
+		for(var i = 0; i < web_clients.length; i++)
+		{
+			console.log("\t" + web_clients[i]['client_id']);
+			web_clients[i]['sock'].write(message);
+		}
+	}
 });
 
 server.listen(2233);
---
+++ web_client.js
@@ -0,0 +1,132 @@
+var http = require('http'),  
+    io = require('socket.io'),
+	net = require('net'); 
+
+Array.prototype.remove = function(from, to) {
+	var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
+};
+
+// list of clients
+var clients = new Array();
+
+var status = null;
+
+server = http.createServer(function(req, res){ 
+ // your normal server code 
+ res.writeHead(200, {'Content-Type': 'text/html'}); 
+ res.end('<h1>Hello world</h1>'); 
+});
+
+server.listen(8080);
+  
+// socket.io 
+var socket = io.listen(server); 
+socket.on('connection', function(client){ 
+	var your_id = new Date().getTime();
+	// create client "object"
+	var client_data = {
+		'client_id': your_id,
+		'client': client
+	};
+	
+	clients.push(client_data);
+	
+	// connect to the suds server
+   /***************************************************************
+   * Connection to SUDS server
+   ***************************************************************/
+	
+	var suds = net.createConnection(2233, '129.21.40.130');
+	suds.addListener('connect', function(){
+		var connect_message = JSON.stringify({'opcode': 'web_connect'});
+
+		suds.write(connect_message);
+	});
+	
+	suds.addListener('data', function(data){
+		console.log(data.toString());
+		var recv = JSON.parse(data.toString());
+		
+		if(recv.opcode == 'suds_status')
+		{
+			status = recv;
+			send_to_all(recv);
+		}
+	});
+	
+	suds.addListener('end', function(){
+		console.log('connection terminated');
+
+	});
+	
+	suds.addListener('error', function(e){
+		console.log('connection ended');
+	});
+	
+	/**************************************************************
+	*
+	***************************************************************/
+	
+	/**************************************************************
+	* Socket.io connection
+	***************************************************************/
+	
+  	client.on('message', function(recv_message){
+		console.log("----------------------------\nsocket.io:");
+		console.log(recv_message);	
+		console.log("----------------------------");
+		if(recv_message.opcode == 'get_status')
+		{
+			client.send(status);
+		}
+				
+	});
+	
+	client.on('disconnect', function(){
+		console.log("----------------------------\nsocket.io:");
+		console.log("Disconnected");
+		console.log("----------------------------");
+		
+		message_to_send = {'opcode': 'message', 'message': '<i>'+client_data['username']+' has disconnected</i>'};
+		send_to_all(message_to_send);
+		
+		for(var i = 0; i < clients.length; i++)
+		{
+			if(clients[i]['client_id'] == your_id)
+			{
+				clients.remove(i);
+			}
+		}
+	});
+	
+	/**
+	 * Send a message to everyone but yourself
+	 */
+	function send_to_peers(message_to_send)
+	{
+		for(var i = 0; i < clients.length; i++)
+		{
+			if(clients[i]['client_id'] != your_id)
+			{
+				clients[i]['client'].send(message_to_send);
+			}
+		}
+	}
+	
+	/**
+	 * Send message to everyone including yourself
+	 */
+	function send_to_all(message_to_send)
+	{
+		//console.log(client);
+		for(var i = 0; i < clients.length; i++)
+		{
+			//console.log(clients[i]['client_id']);
+			clients[i]['client'].send(message_to_send);
+		}
+	}
+	 
+  	 
+});
