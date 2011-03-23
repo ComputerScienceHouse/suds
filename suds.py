6100b880a0fdee128c94a60929f868cce2d79db7
Sean McGary <sean.mcgary@gmail.com>
2011-03-23 15:11:23 -0400

    suds now talks to central server

--- suds.py
+++ suds.py
@@ -2,14 +2,15 @@
 import serial
 import time
 import sys
+import json
 
 from socket import *
 
 # Configuration #
 stallNames = ["South Stairwell - Far", "South Stairwell - Near", "South Vator - Far", "South Vator - Near"]
 
-south_stalls = {0: (1, 'South Stairwell - Far'), 1: (2, 'South Stairwell - Near'), 2: (3, 'South Vator - Far'), 3: (4, 'South Vator - Near')}
-north_stalls = {0: (5, 'North Vator - Far'), 1: (6, 'North Vator - Near'), 2: (7, 'North Stairwell - Far'), 3: (8, 'North Stairwell - Near')}
+south_stalls = ((1, 'South Stairwell - Far'), (2, 'South Stairwell - Near'), (3, 'South Vator - Far'), (4, 'South Vator - Near'))
+north_stalls = ((5, 'North Vator - Far'), (6, 'North Vator - Near'), (7, 'North Stairwell - Far'), (8, 'North Stairwell - Near'))
 
 # Returns a map (from the stall number to it's status) of the stalls that have changed status
 def getChanged(line, prevStatuses):
@@ -36,9 +37,9 @@ if __name__ == '__main__':
 	
 	side = sys.argv[1]
 	if side == 'north':
-		stalls = south_stalls
-	else:
 		stalls = north_stalls
+	else:
+		stalls = south_stalls
 		
 	if len(sys.argv) == 2:
 		port = '/dev/ttyUSB0'
@@ -46,14 +47,17 @@ if __name__ == '__main__':
 		port = sys.argv[2]
 	
 	#connect to the server
-	HOST = '173.84.26.173'
+	HOST = '129.21.40.130'
 	PORT = 2233
 	addr = (HOST, PORT)
 	sock = socket(AF_INET, SOCK_STREAM)
+	buff = 1024
 	sock.connect(addr)
+	connect_message = {'opcode': 'connect', 'suds_id': side, 'data': stalls}
+	sock.send(json.dumps(connect_message, False, True))
 	
 	# Open serial connection
-	ser = serial.Serial(port, timeout=1)
+	ser = serial.Serial(port, timeout = 1)
 	
 	newStatuses = {}
 	prevStatuses = {}	
@@ -65,10 +69,10 @@ if __name__ == '__main__':
 		ser.write('p')
 		newStatuses, prevStatuses = getChanged(ser.readline(), prevStatuses)
 		for i in newStatuses.keys():
-			print(stalls[i][1], newStatuses[i])
-			sock.send(stalls[i][1], newStatuses[i])
-			# Prepare SQL query to INSERT a record into the database.
-			
+			update_stall = {'opcode': 'update_stall', 'suds_id': side, 'stall': stalls[i][0], 'status': newStatuses[i]}
+			print str(update_stall) + "------\n"
+			sock.send(json.dumps(update_stall, False, True))
+			sock.recv(buff);
 		time.sleep(1)
 	
 	ser.close()
--- suds_server.js
+++ suds_server.js
@@ -1,18 +1,63 @@
 var net = require('net');
 
+var stalls = {'south':{}, 'north': {}};
+var connected = {'south': false, 'north': false};
+
 var server = net.createServer(function(socket){
+	var suds_id = null;
+	
 	socket.addListener('connect', function(){
-		console.log('connected');
-		
 		socket.write('connected');
 	});
 	
 	socket.addListener('data', function(data){
-		console.log(data);
+		var recv = JSON.parse(data.toString());
+		
+		if(recv.opcode == 'connect')
+		{
+			connected[recv['suds_id']] = true;
+			suds_id = recv['suds_id'];
+			
+			for(var i = 0; i < recv.data.length; i++)
+			{
+				var d = recv['data'][i];
+				var info = {'name': d[1], 'status': 0};
+				stalls[recv['suds_id']][d[0]] = info;
+			}
+			console.log(recv['suds_id'] + ' connected');
+			//console.log(stalls);
+			socket.write("ACK\n");
+		}
+		else if(recv.opcode == 'update_stall')
+		{
+			var id = recv.suds_id;
+			var stall = recv.stall
+			//console.log(recv);
+			stalls[id][stall]['status'] = recv['status'];
+			console.log(stalls);
+			socket.write("ACK\n");			
+		}
+		else
+		{
+			socket.write("BADACK\n");
+			console.log('')
+		}
+		
 	});
 	
 	socket.addListener('end', function(){
 		console.log('connection terminated');
+		// mark as disconnected
+		connected[suds_id] = false;
+		suds_id = null;
+	});
+	
+	socket.addListener('error', function(e){
+		// mark as disconnected
+		connected[suds_id] = false;
+		stalls[suds_id] = {};
+		console.log(suds_id + " disconnected");
+		suds_id = null;		
 	});
 });
 
