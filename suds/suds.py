40817e72e814c606e3c142c15d3614f427d1adb3
Sean McGary <sean.mcgary@gmail.com>
2011-03-23 22:35:18 -0400

    changing server address

--- suds/suds.py
+++ suds/suds.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 		port = sys.argv[2]
 	
 	#connect to the server
-	HOST = '129.21.40.130'
+	HOST = '129.21.49.139'
 	PORT = 2233
 	addr = (HOST, PORT)
 	sock = socket(AF_INET, SOCK_STREAM)
