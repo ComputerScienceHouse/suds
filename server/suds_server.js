be04d1a08ba0d9f6463b4fba10108d5d30013c1a
Sean McGary <sean.mcgary@gmail.com>
2011-04-17 18:00:05 -0400

    added readme files for web and web_socket_server

--- server/suds_server.js
+++ server/suds_server.js
@@ -28,7 +28,7 @@ var server = net.createServer(function(socket){
 		recv = recv.replace("\\n", "");
 		try
 		{
-			
+			console.log(recv);
 			recv = JSON.parse(recv);
 			//console.log(recv);
 			//var ack_msg = {"opcode" : "ACK"};
